212731d7af3d433c90cae8f563a6d113d40a30be
Rushabh22 <A0135914@u.nus.edu>
2017-06-25 09:11:47 +0800

    Creating service for get list and get single item

--- src/main/java/springboot/topic/TopicController.java
+++ src/main/java/springboot/topic/TopicController.java
@@ -1,20 +1,30 @@
 package springboot.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
+	@Autowired //Mark this with the annotation to do dependency injection.
+	private TopicService topicService;
 
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 
-		return Arrays.asList(new Topic("Spring", "Spring framework", "spring framework description"),
-				new Topic("Java", "Java coursework", "java course description"),
-				new Topic("Javascript", "Javascript coursework", "Javascript descritpion"));
+		List<Topic> topics = topicService.getAllTopics();
+		
+		return topics;
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id){
+		Topic topic = topicService.getTopic(id);
+		return topic;
+		
 	}
 
 }
---
+++ src/main/java/springboot/topic/TopicService.java
@@ -0,0 +1,26 @@
+package springboot.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+/*
+ * @Service : Marks the class as spring business service. 
+   It create instance and registers it as a Singleton
+*/
+@Service 
+public class TopicService {
+	private List<Topic> topics = Arrays.asList(new Topic("Spring", "Spring framework", "spring framework description"),
+			new Topic("Java", "Java coursework", "java course description"),
+			new Topic("Javascript", "Javascript coursework", "Javascript descritpion"));
+
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		Topic topic = topics.stream().filter(t -> t.getId().equalsIgnoreCase(id)).findFirst().get();
+		return topic;
+	}
+}
