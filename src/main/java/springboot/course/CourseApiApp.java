de8951502b314cd2cab98f01895d9235a3efa493
Rushabh22 <A0135914@u.nus.edu>
2017-06-25 08:31:53 +0800

    Added course and topic rest api

--- src/main/java/springboot/course/CourseApiApp.java
+++ src/main/java/springboot/course/CourseApiApp.java
@@ -1,9 +1,15 @@
 package springboot.course;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "springboot")
 public class CourseApiApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SpringApplication.run(CourseApiApp.class, args);
 
 	}
 
